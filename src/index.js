6391c59667997d6990bad6caaece542608d21ec8
Amy Posten <amyposten85@gmail.com>
2021-09-29 15:26:14 -0400

    First deliverable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// write your code here
+const fetchURL = `http://localhost:3000/ramens`;
+const imageDestination = document.getElementById(`ramen-menu`);
+
+function fetchRamen(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => getRamenData(json));
+}
+
+function getRamenData(ramenArray) {
+  ramenArray.map((ramen) => getRamenArray(ramen));
+}
+
+function getRamenArray(ramen) {
+  let ramenImage = document.createElement(`img`);
+  ramenImage.src = ramen.image;
+  imageDestination.appendChild(ramenImage);
+}
+
+fetchRamen(fetchURL);
